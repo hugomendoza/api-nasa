441b6eab2b399fed0ce5f5cd6dc0de1b725b93f2
Hugo Mendoza <hmendoza@serempre.com>
2022-04-07 16:29:42 -0500

    feat: markup and styles
    -Add front to section main, headers and form
    -Add function to switch grid section cards

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const inputGrid = document.getElementById("grid");
+const cards = document.querySelector(".cards")
+
+inputGrid.addEventListener("change", () => {
+  cards.classList.toggle("fdfd")
+})
